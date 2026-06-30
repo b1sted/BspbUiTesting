f9bd3808bcd231884eb441c603e3ab035282785e
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-06-30 14:16:19 +0300

    test: add BaseTest with driver lifecycle

---
+++ src/test/java/ru/basted/bspbuitesting/base/BaseTest.java
@@ -0,0 +1,23 @@
+package ru.basted.bspbuitesting.base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    protected WebDriver webDriver;
+
+    @BeforeEach
+    public void setup() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+}
