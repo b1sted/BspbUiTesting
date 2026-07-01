5f0ea11fa8baca2b97d5988e2bd48c8aedde3d9a
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-07-01 14:33:14 +0300

    refactor: make BasePage and BaseTest abstract

--- src/test/java/ru/basted/bspbuitesting/base/BasePage.java
+++ src/test/java/ru/basted/bspbuitesting/base/BasePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+public abstract class BasePage {
     protected final WebDriver webDriver;
     protected final WebDriverWait webDriverWait;
 
