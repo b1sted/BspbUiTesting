ad7c1a5f4a828a491efd7800b24b4423843aed56
Nikita Mandrykin <189896017+b1sted@users.noreply.github.com>
2026-07-03 15:51:09 +0300

    refactor(base-page): remove unused import

--- src/test/java/ru/basted/bspbuitesting/base/BasePage.java
+++ src/test/java/ru/basted/bspbuitesting/base/BasePage.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
